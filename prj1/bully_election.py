efa0e1279758a7f3560ccf8da70d7e2cf103c9e4
Bjarke <201805703@post.au.dk>
2022-03-03 11:26:57 +0100

    Bully_election v2
    Implemented the rest of bully election, not yet runned

--- prj1/bully_election.py
+++ prj1/bully_election.py
@@ -1,7 +1,25 @@
+from asyncio.windows_events import NULL
 from enum import Enum
+import queue
 import threading
 import time
+import random
 # from mpi4py import MPI
+
+#node_ids = range(MPI.COMM_WORLD.size)
+#highest_id = max(node_ids)
+#comm = MPI.COMM_WORLD
+
+seed = 42
+ID_array = ProcessID()
+election_timer = None
+
+recv_queue = []
+
+election_count = 0
+broadcast_count = 0
+ok_count = 0
+total_count = 0
 
 class Message():
     def __init__(self, sender, type, data = None):
@@ -23,27 +41,92 @@ def Construct_msg(sender, type, data = None):
     return Message(sender=sender,type=type,data=data)
 
 class myTimer():
-    def __init__(self,seconds):
-        self.T = threading.Timer(seconds, Coordinate)
+    def __init__(self,seconds,func):
+        self.T = threading.Timer(seconds, func)
         self.T.start()
 
     def kill(self):
         self.T.cancel()
 
-def Coordinate(): 
-    print("Coordinateing")
+class ProcessID():
+    def __init__(self):
+        random.seed(seed)
+        #self.ID_array = random.sample(range(1,comm.size+20),comm.size)
+        self.ID_array = random.sample(range(1,8+20),8)
+
+    def getID(self, rank):
+        return self.ID_array[rank]
+
+    def getLowestID(self):
+        return min(self.ID_array)
+
+    def __repr__(self):
+        return str({"ID_array": self.ID_array})
+    
+
+def Coordinate(comm): 
+    global broadcast_count
+    for rank in range(comm.size):
+        if rank != comm.rank:
+            comm.send(Construct_msg(comm.rank, Type.COORDINATOR,ID_array.getID(comm.rank)), rank)
+            broadcast_count += 1
+    print(f"Done broadcasting leader on node {comm.rank}.")
+
+def HoldElection():
+    global election_count
+    my_id = ID_array.getID(comm.rank)
+    global election_timer
+    election_timer = myTimer(1,Coordinate)
+
+    for other_rank, id in enumerate(ID_array.ID_array):
+        if id > my_id:
+            comm.send(Construct_msg(comm.rank, Type.ELECTION), other_rank)
+            election_count += 1
+
+def Recieve_Handle(Message):
+    if Massage.type == Type.Election:
+        global ok_count
+        comm.send(Construct_msg(comm.rank, Type.OK),Message.sender)
+        ok_count += 1
+        HoldElection()
+    elif Message.type == Type.OK:
+        global election_timer
+        election_timer.kill()
+    elif Message.type == Type.COORDINATOR:
+        print(f"Node: {ID_array.getID(comm.rank)} acknowledge {ID_array.getID(Message.sender)} as leader")
+        global election_count,broadcast_count, ok_count
+        msg_count = election_count + broadcast_count + ok_count
+        comm.send(Construct_msg(comm.rank,Type.MSG_COUNT,msg_count),Message.sender)
+    elif Message.type == Type.MSG_COUNT:
+        global total_count
+        print(f"Node: {ID_array.getID(comm.rank)} counted {ID_array.getID(Message.data)} from {ID_array.getID(Message.sender)}")
+        total_count += Message.data
+        print(f"Node: {ID_array.getID(comm.rank)} counted {total_count} in total")
 
 
-mt = myTimer(2)        
-time.sleep(5)
-mt.kill()
+def receive():
+    my_id = ID_array.getID(comm.rank)
+    print(f"Node {my_id} ready to recv.")
+    while(True):
+        data = comm.recv()
+        recv_queue.append(data)
 
+def dispatch():
+    while(True):
+        try:
+            next = recv_queue.pop(0)
+        except IndexError:
+            time.sleep(1)
+            continue
+        Recieve_Handle(next)
 
-mt2 = myTimer(2)        
-time.sleep(1)
-mt2.kill()
+def main():
+    if ID_array.getLowestID() == ID_array.getID(comm.rank):
+        HoldElection()
 
-# max_rank = MPI.COMM_WORLD.size - 1
-# comm = MPI.COMM_WORLD
+    recv_t = threading.Thread(target=receive)
+    recv_t.start()
+    dispatch()
 
-
+if __name__ == "__main__":
+    main()
--- prj1/unit_test_bully.py
+++ prj1/unit_test_bully.py
@@ -1,11 +1,65 @@
+from concurrent.futures import thread
+from telnetlib import EL
 import unittest as unit
 import bully_election 
+from time import sleep
+
+class mockCounter():
+    def __init__(self):
+        self.counter = 0
+
+    def incrementCounter(self):
+        self.counter += 1
+
+    def getCounter(self):
+        return self.counter
 
 class Testing(unit.TestCase):
+    def entry(self):
+        global seed, ID_array, election_timer,recv_queue,election_count,broadcast_count,ok_count,total_count
+        seed = 42
+        ID_array = bully_election.ProcessID()
+        election_timer = None
+
+        recv_queue = []
+
+        election_count = 0
+        broadcast_count = 0
+        ok_count = 0
+        total_count = 0
+
     def test_Message(self):
         uut = bully_election.Message(42,bully_election.Type.OK)
         self.assertEqual(uut.sender, 42)
         self.assertEqual(uut.type,bully_election.Type.OK)
+
+    def test_timer(self):
+        mc = mockCounter()
+        self.assertEqual(mc.getCounter(),0) #Assert that counter is zero
+        uut = bully_election.myTimer(1,mc.incrementCounter)
+        self.assertEqual(mc.getCounter(),0)
+        sleep(0.9)
+        self.assertEqual(mc.getCounter(),0)
+        sleep(0.2)
+        self.assertEqual(mc.getCounter(),1)
+
+    def test_timerKill(self):
+        mc = mockCounter()
+        uut = bully_election.myTimer(1,mc.incrementCounter)
+        self.assertEqual(mc.getCounter(),0)
+        uut.kill()
+        sleep(1.1)
+        self.assertEqual(mc.getCounter(),0)
+
+    def test_Coordinate(self):
+        self.entry()
+        init_count = broadcast_count
+        
+
+
+    
+
+
 
 if __name__ == '__main__':
     unit.main()
