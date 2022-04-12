2ff359b43c88acecc3e406286b1e3f8910869a69
Bwarke2 <201805703@post.au.dk>
2022-04-12 16:02:12 +0100

    Removed spaces in names

--- prj2/speakers/speaker_bluetooth.py
+++ prj2/speakers/speaker_bluetooth.py
@@ -3,9 +3,8 @@ from playsound import playsound
 
 #play sound
 file = "TAMMY Stan Dereaux.mp3"
-print('Playing sount\n')
-playsound('prj2\speakers\TAMMY-Stan-Devereaux.mp3')
-
+print('Playing sound \n')
+playsound('/DIPS/prj2/speakers/TAMMY-Stan-Devereaux.mp3')
 
 
 
