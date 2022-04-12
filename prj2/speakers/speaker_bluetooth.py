e77e368ab9665cd9f016c307c30c8c2227a3d52b
Bjarke <201805703@post.au.dk>
2022-04-12 16:37:56 +0200

    Added test program

---
+++ prj2/speakers/speaker_bluetooth.py
@@ -0,0 +1,13 @@
+import os
+from playsound import playsound
+
+#play sound
+file = "TAMMY Stan Dereaux.mp3"
+print('Playing sount\n')
+playsound('prj2\speakers\TAMMY-Stan-Devereaux.mp3')
+
+
+
+
+
+
