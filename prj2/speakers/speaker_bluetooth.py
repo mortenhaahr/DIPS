0a5de9a1c86516bc707719c15dff97dacaeb452c
Bjarke <201805703@post.au.dk>
2022-04-16 11:53:37 +0200

    Update speaker_bluetooth.py
    Using pygame instead

--- prj2/speakers/speaker_bluetooth.py
+++ prj2/speakers/speaker_bluetooth.py
@@ -1,12 +1,20 @@
 import os
-from playsound import playsound
+#from playsound import playsound
+
+import pygame
+
+pygame.mixer.init()
 
 #play sound
-file = "TAMMY Stan Dereaux.mp3"
+file = 'Tammy-Stan-Devereaux.mp3'
+pygame.mixer.music.load(file)
+
 print('Playing sound \n')
-playsound('/DIPS/prj2/speakers/TAMMY-Stan-Devereaux.mp3')
+pygame.mixer.music.play(loops=-1)
 
+print("Volume is: ", pygame.mixer.music.get_volume(),"\n")
 
-
+while pygame.mixer.music.get_busy() == True:
+    continue
 
 
