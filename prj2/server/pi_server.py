46e171ac2a5e59b353b3181d3c20009f5b869a2f
cakemaster9001 <fisk9889@gmail.com>
2022-04-21 11:51:18 +0200

    Getting a topic working

--- prj2/server/pi_server.py
+++ prj2/server/pi_server.py
@@ -23,8 +23,8 @@ def main():
 	client.connect("localhost", 1883)  # Mortens laptop IP
 	client.subscribe("pi_server/#")
 	client.subscribe("pi_server/mood_detector", callback=mood_callback)
+	
 	client.loop_forever()
-
 
 if __name__ == "__main__":
 	
