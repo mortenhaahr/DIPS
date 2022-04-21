797d082f16bdd61fb306fe9123e0b26a0e3d167b
cakemaster9001 <fisk9889@gmail.com>
2022-04-21 11:24:06 +0200

    Initial mood_callback

--- prj2/server/pi_server.py
+++ prj2/server/pi_server.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
 import logging
 import threading
+
+from sqlalchemy import null
 from mqtt_callback_client import MQTTCallbackClient
 from colorsys import hsv_to_rgb
 
-import socket
 
+client = null
+
+def mood_callback(payload):
+	global client
+	print("I received a mood!")
+	logging.info(payload)
 
 
 def main():
-	socket_ip = socket.gethostbyname(socket.gethostname())
-	print (f"ip = {socket_ip}")
+	global client
 	logging.basicConfig(
 	    level=logging.INFO, format="%(asctime)s : %(levelname)s:  %(message)s"
 	)
-	return
 	client = MQTTCallbackClient(client_id="Kubuntu_sub", userdata="DumDumReceiver")
-	client.connect("192.168.0.106", 1883)  # Mortens laptop IP
-	client.subscribe("mortens_sick_bee/#")
-	client.subscribe("mortens_sick_bee/pir_sensor", callback=led_callback)
+	client.connect("localhost", 1883)  # Mortens laptop IP
+	client.subscribe("pi_server/#")
+	client.subscribe("pi_server/mood_detector", callback=mood_callback)
 	client.loop_forever()
 
 
 if __name__ == "__main__":
+	
 	main()
