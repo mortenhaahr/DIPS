89b7501331a0f5bb5466ce24525a29aefcd3c059
cakemaster9001 <fisk9889@gmail.com>
2022-04-21 11:16:28 +0200

    Socket_ip local var

--- prj2/server/pi_server.py
+++ prj2/server/pi_server.py
@@ -7,17 +7,20 @@ from colorsys import hsv_to_rgb
 import socket
 
 
+
 def main():
-    logging.basicConfig(
-        level=logging.INFO, format="%(asctime)s : %(levelname)s:  %(message)s"
-    )
-    client = MQTTCallbackClient(client_id="Kubuntu_sub", userdata="DumDumReceiver")
-    client.connect("192.168.0.106", 1883)  # Mortens laptop IP
-    client.subscribe("mortens_sick_bee/#")
-    client.subscribe("mortens_sick_bee/pir_sensor", callback=led_callback)
-    client.loop_forever()
+	socket_ip = socket.gethostbyname(socket.gethostname())
+	print (f"ip = {socket_ip}")
+	logging.basicConfig(
+	    level=logging.INFO, format="%(asctime)s : %(levelname)s:  %(message)s"
+	)
+	return
+	client = MQTTCallbackClient(client_id="Kubuntu_sub", userdata="DumDumReceiver")
+	client.connect("192.168.0.106", 1883)  # Mortens laptop IP
+	client.subscribe("mortens_sick_bee/#")
+	client.subscribe("mortens_sick_bee/pir_sensor", callback=led_callback)
+	client.loop_forever()
 
 
 if __name__ == "__main__":
-	print(socket.gethostbyname(socket.gethostname()))
 	main()
