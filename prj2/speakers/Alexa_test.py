3ed9eebcad94a21c32dd3180b86020499e65aacc
Bjarke <201805703@post.au.dk>
2022-04-28 12:05:24 +0200

    Added happy

--- prj2/speakers/Alexa_test.py
+++ prj2/speakers/Alexa_test.py
@@ -1,13 +1,14 @@
 import logging
 import os
+from time import time
 from unittest import case
+
+from arrow import now
  
 from flask import Flask
-from flask_ask import Ask, request, session, question, statement
+import json
  
 app = Flask(__name__)
-ask = Ask(app, "/")
-logging.getLogger('flask_ask').setLevel(logging.DEBUG)
  
 STATUSON = ["on", "switch on", "enable", "power on", "activate", "turn on"] # all values that are defined as synonyms in type
 STATUSOFF = ["off", "switch off", "disactivate", "turn off", "disable", "turn off"]
@@ -17,50 +18,91 @@ MoodsSAD = ["Sad", "Dejected", "Gloomy", "Down", "Dejected", "Miserable", "Low-s
 MoodsANGRY = ["Angry", "Wrathful", "Irate", "Seething", "Livid", "Furious", "Sore", "Mad", "Irate", "Cross", "Frustrated", "Pissed off"]
 MoodsHAPPY = ["in a good mood", "Pleased", "Cheerfull", "Glad"]
 
+json_testDic = {
+        "type": "IntentRequest",
+        "requestId": "requestId",
+        "timestamp": "timestamp",
+        "dialogState": "string",
+        "locale": "en-US",
+        "intent": {
+            "name": "Mood_Intent",
+            "confirmationStatus": "string",
+            "slots": {
+            "SlotName": {
+                "name": "Mood",
+                "value": "string",
+                "confirmationStatus": "string",
+                "slotValue": {},
+                "resolutions": {
+                "resolutionsPerAuthority": [
+                    {
+                    "authority": "string",
+                    "status": {
+                        "code": "string"
+                    },
+                    "values": [
+                        {
+                        "value": {
+                            "name": "string",
+                            "id": "string"
+                        }
+                        }
+                    ]
+                    }
+                ]
+                }
+            }
+            }
+        }
+    }
 
-@ask.launch
-def launch():
-    speech_text = 'Welcome to the Raspberry Pi alexa automation.'
-    return question(speech_text).reprompt(speech_text).simple_card(speech_text)
- 
-@ask.intent('LightIntent', mapping = {'status':'status'})
-def Gpio_Intent(status,room):
-    if status in STATUSON:
-        return statement('Light was turned on')
-    elif status in STATUSOFF:
-        return statement('Light was turned off')
-    else:
-        return statement('Sorry, this command is not possible.')
- 
-@ask.intent('MoodIntent', mapping = {'Mood':'Mood'})
-def Mood_Intent(Mood,room):
-    if Mood in MoodsNEUTRAL:
-        return statement('Playing neutral music')
-    elif Mood in MoodsSAD:
-        return statement('Playing sad music')    
-    elif Mood in MoodsANGRY:
-        return statement('Playing angry music')    
-    elif Mood in MoodsHAPPY:
-        return statement('Playing happy music')
-    else:
-        return statement('Sorry, you are in a strange mood')
+
+#Launch Request
+def launch_req(requestId):
+    return{
+        "type": "LaunchRequest",
+        "requestId": requestId,
+        "timestamp": time(),
+        "locale": "en-US"
+    }
 
 
 
-@ask.intent('AMAZON.HelpIntent')
-def help():
-    speech_text = 'You can say hello to me!'
-    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
- 
- 
-@ask.session_ended
-def session_ended():
-    return "{}", 200
+
+def get_emosion(jsonInput):
+    json_obj = json.loads(jsonInput)
+    intent = json_obj["intent"]
+    slot = intent["slots"]
+    if intent["name"] == "Mood_intent":
+        return intent.slots.Mood
+
+
+    
+
+
+
+
+##@ask.intent('MoodIntent', mapping = {'Mood':'Mood'})
+#def Mood_Intent(Mood,room):
+#    if Mood in MoodsNEUTRAL:
+#        return statement('Playing neutral music')
+#    elif Mood in MoodsSAD:
+#        return statement('Playing sad music')    
+#    elif Mood in MoodsANGRY:
+#        return statement('Playing angry music')    
+#    elif Mood in MoodsHAPPY:
+#        return statement('Playing happy music')
+#    else:
+#        return statement('Sorry, you are in a strange mood')
+
+
+
+#@ask.session_ended
+#def session_ended():
+#    return "{}", 200
  
  
 if __name__ == '__main__':
-    if 'ASK_VERIFY_REQUESTS' in os.environ:
-        verify = str(os.environ.get('ASK_VERIFY_REQUESTS', '')).lower()
-        if verify == 'false':
-            app.config['ASK_VERIFY_REQUESTS'] = False
+    json_testFile = json.dumps(json_testDic)
+    emotion = get_emosion(json_testFile)
     app.run(debug=True)
