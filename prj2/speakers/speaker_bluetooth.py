37f29aff084b7b5b7f5b17b8eeaafcc591ac9ea6
Bjarke <201805703@post.au.dk>
2022-05-02 15:02:28 +0200

    Update speaker_bluetooth.py
    removed subscribe

--- prj2/speakers/speaker_bluetooth.py
+++ prj2/speakers/speaker_bluetooth.py
@@ -78,9 +78,10 @@ class Speaker():
             self.stopMusic()
 
     def setupTopics(self,client):
-        client.subscribe("pi_server/context/emotion", callback=self.emotion_callback)#,qos=1)
-        client.subscribe("pi_server/context/room" + "1", callback=lambda payload: self.room_callback(payload, 1))
-        client.subscribe("pi_server/context/room" + "2", callback=lambda payload: self.room_callback(payload, 2))
+        pass
+        #client.subscribe("pi_server/context/emotion", callback=self.emotion_callback)#,qos=1)
+        #client.subscribe("pi_server/context/room" + "1", callback=lambda payload: self.room_callback(payload, 1))
+        #client.subscribe("pi_server/context/room" + "2", callback=lambda payload: self.room_callback(payload, 2))
 
 
 def main():
