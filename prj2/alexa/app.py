0f641fa7635ce04ed52e99e4d78f47dfea82d29a
Morten Haahr Kristensen <mortenhaahrkristensen@gmail.com>
2022-05-03 09:41:04 +0200

    Now supporting yes and no

--- prj2/alexa/app.py
+++ prj2/alexa/app.py
@@ -1,4 +1,3 @@
-from concurrent.futures import thread
 from flask import Flask, request
 import json
 from mqtt_callback_client import MQTTCallbackClient
@@ -9,6 +8,7 @@ import logging
 base_topic = "pi_server"
 client = None
 context = {}
+last_command = ""
 
 
 def get_context(topic, payload):
@@ -46,69 +46,75 @@ def create_basic_text_response(
     }
 
 
-basic_response = {
-    "version": "1.0",
-    "sessionAttributes": {
-        # "supportedHoroscopePeriods": {
-        #     "daily": True,
-        #     "weekly": False,
-        #     "monthly": False,
-        # }
-    },
-    "response": {
-        "outputSpeech": {
-            "type": "PlainText",
-            "text": f"I'm sorry I didn't quite get that. Try saying HELP.",
-        },
-        # "card": {
-        #     "type": "Simple",
-        #     "title": "Mood",
-        #     "content": "Today will provide you a new learning opportunity.  Stick with it and the possibilities will be endless.",
-        # },
-        # "reprompt": {
-        #     "outputSpeech": {
-        #         "type": "PlainText",
-        #         "text": "Can I help you with anything else?",
-        #     }
-        # },
-        "shouldEndSession": False,
-    },
-}
-
 app = Flask(__name__)  # __name__ = filename
 
 
 def launch_handler(data_request):
-    global context
+    global context, last_command
     try:
         emotion = context["emotion"]["emotion"]
     except KeyError:
         emotion = "undefined"
     response = create_basic_text_response(
-        f"""Hi there! Your current emotion is set to {emotion}. You can change it by saying: "Change my emotion to happy". I currently support the emotions "angry", "happy", "sad" and "neutral"."""
+        f"""Your current emotion is set to {emotion}. I support the emotions "angry", "happy", "sad" and "neutral". Do you want to change your mood?"""
     )
+    last_command = "launch"
     return json.dumps(response)
 
 
 def set_emotion_callback(intent):
+    global last_command
     emotion = intent["slots"]["emotion"]["value"]
     emotion_topic = f"{base_topic}/emotion"
     client.publish(emotion_topic, json.dumps({"emotion": emotion}))
-    response = create_basic_text_response(f"""Changing your emotion to {emotion}.""")
+    response = create_basic_text_response(
+        f"""Changing your emotion to {emotion}.""", end_session=True
+    )
+    last_command = "get_emotion"
     return json.dumps(response)
 
 
 def get_emotion_callback(intent):
+    global context, last_command
+    try:
+        emotion = context["emotion"]["emotion"]
+    except KeyError:
+        emotion = "undefined"
     response = create_basic_text_response(
-        f"""Your mood is currently set to {context.get("emotion", "undefined")}. Do you want to change it?"""
+        f"""Your emotion is currently set to {emotion}. Do you want to change it?"""
     )
+    last_command = "get_emotion"
     return json.dumps(response)
+
+
+def yes_handler(intent):
+    global last_command
+    old_last_command = last_command
+    last_command = "yes"
+    if old_last_command == "get_emotion" or old_last_command == "launch":
+        return json.dumps(create_basic_text_response("All right. How do you feel?."))
+
+    return json.dumps(
+        create_basic_text_response(
+            "I'm sorry I can't let you do that Dave.", end_session=True
+        )
+    )
+
+
+def no_handler(intent):
+    global last_command
+    last_command = "no"
+    return json.dumps(
+        create_basic_text_response("All right. Talk to you soon.", end_session=True)
+    )
 
 
 def intent_handler(data_request):
     intents_callbacks = {
         "set_emotion": set_emotion_callback,
         "get_emotion": get_emotion_callback,
+        "AMAZON.YesIntent": yes_handler,
+        "AMAZON.NoIntent": no_handler,
     }
     intent = data_request["intent"]["name"]
     print("Intent handler called")
@@ -117,8 +123,13 @@ def intent_handler(data_request):
 
 
 def default_command(data_request):
+    global last_command
     print("Default command called")
-    return json.dumps(basic_response)
+    response = create_basic_text_response(
+        "I'm sorry I didn't quite get that. Try saying HELP."
+    )
+    last_command = "default"
+    return json.dumps(response)
 
 
 request_types = {
